b17c653c24405254283def427669a4df26a70660
Netta Steinberg <netta.steinberg@gmail.com>
2023-06-17 15:05:22 +0300

    Added 'api/' to the end of the render route

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:8001/api/" : "https://go-code-shop-full-project.onrender.com/";
+export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:8001/api/" : "https://go-code-shop-full-project.onrender.com/api/";
 export const GET_OR_DELETE_OR_EDIT_PRODUCT_BY_ID = BASE_URL + "product/";
 
 /* When building the client inside the server with 'npm run build', for some reason (bug?) NODE_ENV === 'production', so the client is unable to

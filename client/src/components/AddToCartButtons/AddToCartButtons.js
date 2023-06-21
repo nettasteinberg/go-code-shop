1770ecb503fa2c679e58c67600fb5b4a27a7970a
Netta Steinberg <netta.steinberg@gmail.com>
2023-06-21 14:01:45 +0300

    Added additional style to the 'add/remove from cart' buttons

--- client/src/components/AddToCartButtons/AddToCartButtons.js
+++ client/src/components/AddToCartButtons/AddToCartButtons.js
@@ -1,8 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { MyContext } from '../../MyContext';
 import "./AddToCartButtons.css"
+import add_to_cart_image from '../../Icons/add_to_cart.jpg';
+import subtract_from_cart_image from '../../Icons/subtract_from_cart.jpg';
+import remove_from_cart_image from '../../Icons/remove_from_cart.jpg';
 
-const AddToCartButtons = ({ id, title, price, image}) => {
+const AddToCartButtons = ({ id, title, price, image }) => {
     const { incrementProduct, decrementProduct, addToCart, itemsInCart, setItemsInCart } = useContext(MyContext);
     const [count, setCount] = useState(0);
 
@@ -12,8 +15,12 @@ const AddToCartButtons = ({ id, title, price, image}) => {
         }
         console.log(`Removing product "${itemsInCart[id]["name"]}" from the cart`);
         delete itemsInCart[id];
-        setItemsInCart({...itemsInCart});
+        setItemsInCart({ ...itemsInCart });
+        setCount(0);
     }
+
+    const addOnClick = () => { addToCart(id, title, price, image, count, setCount); setItemsInCart({ ...itemsInCart }); };
+    const removeOnClick = () => { removeFromCart(id) };
 
     return (
         <React.Fragment>
@@ -24,10 +31,28 @@ const AddToCartButtons = ({ id, title, price, image}) => {
                     <button className='plus' onClick={() => incrementProduct(setCount)}>+</button>
                 </div>
                 <div className="addToCart">
-                    <button className="button add" onClick={() => {addToCart(id, title, price, image, count, setCount); setItemsInCart({...itemsInCart});}}> {count >= 0 ? "Add to cart" : "Subtract from cart"} </button>
+                    <img
+                        src={count >= 0 ? add_to_cart_image : subtract_from_cart_image}
+                        alt={count >= 0 ? 'add to cart icon' : 'subtract from cart icon'}
+                        height={32}
+                        width={32}
+                        onClick={addOnClick}
+                        className='hover'
+                    />
+                    <button className="button add hover" onClick={addOnClick}>
+                        {count >= 0 ? "Add to cart" : "Subtract from cart"}
+                    </button>
                 </div>
                 <div className="removeFromCart">
-                    <button className="button remove" onClick={() => {removeFromCart(id)}}>Remove from cart</button>
+                    <button className="button remove hover" onClick={removeOnClick}>Remove from cart</button>
+                    <img
+                        src={remove_from_cart_image}
+                        alt='remove from cart icon'
+                        height={32}
+                        width={32}
+                        onClick={removeOnClick}
+                        className='hover'
+                    />
                 </div>
             </div>
         </React.Fragment>
